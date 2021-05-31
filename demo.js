99cc82eaefbaca73d380ef54424318df6890b224
Richard Ng <richardcrng@gmail.com>
2021-05-31 20:38:56 +0100

    Add demo language files

---
+++ demo.js
@@ -0,0 +1,6 @@
+"use strict"; // turns strict mode on
+
+// put your throwaway code below and run with node demo.js
+
+let myFaveNumber = 10;
+let myFaveNumber = 42;
---
+++ demo.py
@@ -0,0 +1,15 @@
+# put your throwaway code below and run with python demo.py
+
+first_number = 5
+second_number = 10
+
+if first_number == second_number:
+
+# should indent to come inside an if block!
+print("The two numbers are equal!")
+
+# shouldn't randomly indent an else!
+				else:
+
+		# honestly this is just a mess
+		print("The two numbers are not equal.")
